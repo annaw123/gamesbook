07be8abc00472e2eb77545eea20742d71ba18977
annaw123 <14wakea@clsg.org.uk>
2019-08-17 12:58:18 +0100

    Add Guess the Number game

---
+++ guess the number.py
@@ -0,0 +1,23 @@
+import random
+
+guessesTaken = 0
+print('Hello, What is your name?')
+name = input()
+
+number = random.randint(1, 20)
+print("Well, %s, I am thinking of a number between 1 and 20" % name)
+
+for guessesTaken in range(6):
+    print('Take a guess')
+    guess = int(input())
+
+    if guess < number:
+        print('Too low ma friend')
+
+    if guess > number:
+        print('Too high ma friend')
+
+    if guess == number:
+        guessesTaken = str(guessesTaken + 1)
+        print("Good job, %s, you guessed my number in %s guesses!" % (name, guessesTaken))
+        exit()
